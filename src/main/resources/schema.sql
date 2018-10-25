2164fd87df8df54761111d62cc51a7e97427a8a7
Rafno <rafnar1993@gmail.com>
2018-10-25 10:30:28 +0000

    Bætt við todo að eyða schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 -- TODO REFACTOR SO THIS SCRIPT DOES NOT EXIST.
 -- Spring automatically creates a schema and orders it alphabetically.
+-- Scheme is now imported alphabetically, TODO delete this
 
-
-DROP TABLE IF EXISTS medicine;
-CREATE TABLE public.medicine (
-    id integer NOT NULL,
-    name character varying(5000),
-    active_ingredient character varying(5000),
-    pharmaceutical_form character varying(5000),
-    strength character varying(5000),
-    atc_code character varying(5000),
-    legal_status character varying(5000),
-    mah character varying(5000),
-    other_info character varying(8000),
-    marketed character varying(5000),
-    ma_issued character varying(5000)
-);
+-- DROP TABLE IF EXISTS medicine;
+-- CREATE TABLE public.medicine (
+--    id integer NOT NULL,
+--    name character varying(5000),
+--    active_ingredient character varying(5000),
+--    pharmaceutical_form character varying(5000),
+--    strength character varying(5000),
+--    atc_code character varying(5000),
+--    legal_status character varying(5000),
+--    mah character varying(5000),
+--    other_info character varying(8000),
+--    marketed character varying(5000),
+--    ma_issued character varying(5000)
+--);
