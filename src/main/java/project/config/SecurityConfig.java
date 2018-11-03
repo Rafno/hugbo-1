d83600c58a378b7b1f37e5468bace38a288c00a2
Rafno <rafnar1993@gmail.com>
2018-11-03 21:13:25 +0000

    Lagað villu með að sjá myhome

--- src/main/java/project/config/SecurityConfig.java
+++ src/main/java/project/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -40,25 +41,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
+		
 		http
 			.authorizeRequests()
-			.antMatchers("/login*").anonymous()
-			.anyRequest().authenticated()
-			.and()
-			.formLogin()
-			.loginPage("/login")
-			.defaultSuccessUrl("/")
-			.failureUrl("/login.html?error=true")
-			.and()
-			.logout().logoutSuccessUrl("/login.html");
-		
-		
-		/*
-		http
-			.authorizeRequests()
-			.antMatchers("/myHome").hasRole("USER")
-			
-			.and()
+			.antMatchers("/").permitAll()
+			.antMatchers("/about").permitAll()
+			.antMatchers("/login").permitAll()
+			.antMatchers("/about").permitAll()
+			.antMatchers("/registration").permitAll()
+			.antMatchers("/myHome").hasAuthority("USER").anyRequest()
+			.authenticated().and().csrf().disable()
 			.formLogin()
 			.loginPage("/login")
 			.usernameParameter("username")//
@@ -77,7 +69,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 			
 			.and()
 			.csrf().disable();
-			*/
+			
+	}
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web
+			.ignoring()
+			.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
 	}
 	
 }
