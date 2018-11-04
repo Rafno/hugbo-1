346a4a7645229a36fc17aeeb1b825cf0a91be916
Rafno <rafnar1993@gmail.com>
2018-11-04 18:10:31 +0000

    reynt að bæta við Error handler

--- src/main/java/project/controller/ErrorController.java
+++ src/main/java/project/controller/ErrorController.java
@@ -1,39 +1,30 @@
 package project.controller;
 
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
-public class ErrorController {
-	
-	@RequestMapping(value = "errors", method = RequestMethod.GET)
-	public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+public class ErrorController implements ErrorViewResolver
+{
+	//TODO, this does absolutely nothing.
+	@Override
+	public ModelAndView resolveErrorView(HttpServletRequest request,
+										 HttpStatus status, Map<String, Object> model) {
 		
 		ModelAndView errorPage = new ModelAndView("errorPage");
 		String errorMsg = "";
-		int httpErrorCode = getErrorCode(httpRequest);
 		
-		switch (httpErrorCode) {
-			case 400: {
-				errorMsg = "Http Error Code: 400. Bad Request";
-				break;
-			}
-			case 401: {
-				errorMsg = "Http Error Code: 401. Unauthorized";
-				break;
-			}
-			case 404: {
-				errorMsg = "Http Error Code: 404. Resource not found";
-				break;
-			}
-			case 500: {
 				errorMsg = "Http Error Code: 500. Internal Server Error";
-				break;
-			}
-		}
+				
+			
+		
 		errorPage.addObject("errorMsg", errorMsg);
 		return errorPage;
 	}
---
+++ src/main/webapp/WEB-INF/jsp/ErrorPage/errorPage.jsp
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="ISO-8859-1" %>
+<jsp:include page="../headNavigation/headNavigation.jsp" />
+
+<head>
+    <title>Project Title</title>
+    <link rel="stylesheet" type="text/css" href="<c:url value="/css/myArea.css"/>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<h1>${errorMsg}</h1>
+</body>
+</html>
